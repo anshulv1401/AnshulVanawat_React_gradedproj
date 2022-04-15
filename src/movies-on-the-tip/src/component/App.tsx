206a8e6640a6530fa9fcefafbcc75339780b1745
anshulv1401 <anshulv1401@gmail.com>
2022-04-16 00:36:30 +0530

    About page added

--- src/movies-on-the-tip/src/component/App.tsx
+++ src/movies-on-the-tip/src/component/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Route } from 'react-router';
-import Home from './global/Home';
+import { Route, Switch } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import Navigation from './global/Navigation';
-
+import Home from './global/Home';
+import About from './global/About';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -15,15 +15,27 @@ function App() {
   return (
     <>
     <Navigation />
+    {/* <Container className="my-4">
+      <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/movies-in-theaters" component={MoviesList} />
+          <Route path="/movies-coming" component={MoviesList} />
+          <Route path="/top-rated-india" component={MoviesList} />
+          <Route path="/top-rated-movies" component={MoviesList} />
+          <Route path="/favourite" component={MoviesList} />
+          <Route path="/movies-in-theaters/:id" component={MoviesList} />
+          <Route path="/" component={Home} />
+      </Switch>
+    </Container> */}
+
     <Container className="my-4">
-        <Route path="/" exact>
-            <Home />
-        </Route>
+        <Route path="/" component={Home} exact/>
         <Route path="/movies-in-theaters" component={MoviesList} exact />
         <Route path="/movies-coming" component={MoviesList} exact />
         <Route path="/top-rated-india" component={MoviesList} exact />
         <Route path="/top-rated-movies" component={MoviesList} exact />
         <Route path="/favourite" component={MoviesList} exact />
+        <Route path="/about" component={About} exact />
         <Route path="/:moviesCategory?title=:title" component={MoviesDetails} exact/>
         <Route path="/:moviesCategory/:id" component={MoviesDetails} exact/>
     </Container>
