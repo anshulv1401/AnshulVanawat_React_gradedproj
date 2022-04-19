04210db985e357f1eea18aa3db3c5a15731c4fdd
anshulv1401 <anshulv1401@gmail.com>
2022-04-19 22:41:47 +0530

    Content added in home and about page

--- src/movies-on-the-tip/src/component/global/Home.tsx
+++ src/movies-on-the-tip/src/component/global/Home.tsx
@@ -7,7 +7,9 @@ const Home = () => {
                 </header>
                 <hr />
                 <p>
-                    Movies on the Tip is an online movie manager whose responsibility is to take care of all the activities you can do. 
+                    Movies on the Tip is an online movie manager. Here you can brower through a list of movies under category "Movies in theaters", "Coming Soon", "Top Rated Indian Movies" and "Top Rated Movies".
+                    You can navigate to details of a movie where you'll find information like story line, duration, rating, release date, etc.
+                    You can add or remove a movie from your favourite list and see in under the "Favourite Movie" category. 
                 </p>
             </section>
         </main>
